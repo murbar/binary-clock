41612e04bea6b9a7f72ce0c8b15672dc5edfd37c
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2020-01-07 15:33:26 -0500

    Display time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import useInterval from 'useInterval';
+
+const getTimeBits = () => {
+  const now = new Date();
+  const hrs = now
+    .getHours()
+    .toString(2)
+    .padStart(4, '0');
+  const mins = now
+    .getMinutes()
+    .toString(2)
+    .padStart(6, '0');
+  const secs = now
+    .getSeconds()
+    .toString(2)
+    .padStart(6, '0');
+  return [hrs, mins, secs];
+};
 
 function App() {
-  return <div className="App">Binary clock</div>;
+  const [[hrs, mins, secs], setTime] = React.useState(getTimeBits());
+
+  useInterval(() => {
+    setTime(getTimeBits());
+  }, 200);
+
+  return (
+    <div className="App">
+      {hrs}:{mins}:{secs}
+    </div>
+  );
 }
 
 export default App;
